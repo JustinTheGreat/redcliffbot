39156380ae3102a3e27d2d1eeea5b0667762a1cb
Justin <41762976+JustinTheGreat@users.noreply.github.com>
2023-07-06 21:40:08 -0400

    Update $ranks command

--- redcliffbot.py
+++ redcliffbot.py
@@ -23,7 +23,7 @@ async def on_message(message): #on_message = if bot receives message
     if message.content == '$spreadsheet':
         await message.channel.send('The following is the link to the master spreadsheet for our great clan:\nhttps://docs.google.com/spreadsheets/d/1giyViPKWS6dpsQHy4O3Lu-cEJuQuJ2ErvLfjJhRc2pc/edit')
     if message.content == '$ranks':
-        await message.channel.send('`Ranks\n`')
+        await message.channel.send('```--- Ranks ---\n -- HICOM -- \nCommandant - Obtained by default succession\nAssistant Commandant - Obtained by election\nGrand General - Obtained by 5 months active service and approval from CMDT & ACMDT\nGeneral - Obtained by 4 months active service\nColonel - Obtained by 3 months active service\nLieutenant Colonel - Obtained by 2 months active service\nMajor - Obtained by 1 month active service\n\n -- Commissioned Officers -- \nCaptain - 45 HP & 45 MP\nVice Captain - 35 HP & 35 MP\nFirst Lieutenant - 25 HP & 25 MP\nSecond Lieutenant - 15 HP & 15 MP\nEnsign - 5 HP, 15 CH, & 5 MP + interview\n\n -- Non-Commissioned Officers -- \nStaff Sergeant - 10 PP & 10 CH\nSergeant - 10 PP & 3 CH\nCorporal - 7 PP\n\n -- Enlisted Personel -- \nLance Corporal - 5 PP\nPrivate First Class- 3 PP\nPrivate - 1 PP\nRecruit - Obtained Upon joining/enlisting\n\n -- Points -- \nPP - Promotion Points (Obtained from attending events)\nCH - Co-Host Point (Obtained from co-hosting with hosts)\nHP - Host Point (Obtained from hosting an event)\nMP - Management Point (Obtained from helping to manage roles in #join-request)```')
     #Add Reactions - Copy/Paste or Unicode
     if message.content.startswith('$poop'):
         await message.channel.send('<@&1108598722448863283>')
